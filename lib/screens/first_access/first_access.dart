f02175292d4e55fbaa654c04ae51c33333fc82e9
h4vismat <h4vismat@pm.me>
2025-03-01 00:25:53 -0300

    Created screen for application's first access.

---
+++ lib/screens/first_access/first_access.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:mooze_mobile/widgets/buttons.dart';
+
+class FirstAccessScreen extends StatelessWidget {
+  const FirstAccessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle defaultStyle = TextStyle(color: Colors.white, fontSize: 16.0);
+    TextStyle linkStyle = TextStyle(color: Colors.pinkAccent, fontSize: 16.0);
+
+    final buttons = Column(
+      children: [
+        PrimaryButton(
+          text: "Negociar agora",
+          onPressed: () {
+            Navigator.pushNamed(context, '/create-new-wallet');
+          },
+          icon: Icons.swap_horiz,
+        ),
+        SizedBox(height: 20),
+        TertiaryButton(
+          text: "Criar carteira",
+          onPressed: () {
+            Navigator.pushNamed(context, '/create-new-wallet');
+          },
+          icon: Icons.wallet_rounded,
+        ),
+        SizedBox(height: 20),
+        RichText(
+          text: TextSpan(
+            style: defaultStyle,
+            children: <TextSpan>[
+              TextSpan(text: "Já tem uma carteira? "),
+              TextSpan(
+                text: "Importe-a.",
+                style: linkStyle,
+                recognizer:
+                    TapGestureRecognizer()
+                      ..onTap =
+                          () => Navigator.pushNamed(context, "/import-wallet"),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+
+    final body = Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/mooze-logo.png', width: 200, height: 200),
+          SizedBox(height: 50),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Column(children: [buttons]),
+          ),
+        ],
+      ),
+    );
+
+    return Scaffold(backgroundColor: Color(0xFF14181B), body: body);
+  }
+}
