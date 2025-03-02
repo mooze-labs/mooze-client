69618a7c5b56651a58bf8d7f0f68d2ee1c6f0ba2
h4vismat <h4vismat@pm.me>
2025-03-02 01:47:49 -0300

    Class for mnemonic interaction with FlutterSecureStorage
    Developed class for mnemonic generation and secure persistence with
    FlutterSecureStorage(). This is an effort to remove the usage of the
    mnemonic_provider.dart file as direct in-memory access may be unsafe.

---
+++ lib/utils/mnemonic.dart
@@ -0,0 +1,36 @@
+import 'package:bip39_mnemonic/bip39_mnemonic.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+/// Class to interact with wallet mnemonics.
+class MnemonicSetup {
+  final _storage = const FlutterSecureStorage();
+
+  Future<String> createNewBip39Mnemonic(
+    String walletId,
+    bool extendedPhrase,
+  ) async {
+    final int entropyLength = (extendedPhrase == true) ? 256 : 128;
+    final mnemonic =
+        Mnemonic.generate(
+          Language.portuguese,
+          entropyLength: entropyLength,
+        ).sentence;
+
+    await _saveMnemonic(walletId, mnemonic);
+    return mnemonic;
+  }
+
+  Future<void> _saveMnemonic(String walletId, String mnemonic) async {
+    await _storage.write(key: "mnemonic_$walletId", value: mnemonic);
+  }
+
+  Future<String?> retrieveWalletMnemonic(String walletId) async {
+    final mnemonic = await _storage.read(key: "mnemonic_$walletId");
+
+    return mnemonic;
+  }
+
+  Future<void> deleteMnemonic(String walletId) async {
+    await _storage.delete(key: "mnemonic_$walletId");
+  }
+}
