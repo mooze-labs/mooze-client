ce898f7bcfd7fef3c8b5d4a8522c832486d4117c
h4vismat <h4vismat@pm.me>
2025-03-07 10:37:31 -0300

    New providers for fiat prices, alongside with provider for total wallet balance.

--- lib/providers/external/price_provider.dart
+++ lib/providers/external/price_provider.dart
@@ -3,27 +3,48 @@ import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
-/// A StreamProvider that fetches BTC price in BRL every minute.
-final bitcoinPriceProvider = StreamProvider.autoDispose<double>((ref) {
-  final periodicStream = Stream<void>.periodic(const Duration(minutes: 1));
+// A provider to hold the list of coin IDs to query
+final cryptoPriceConfigProvider = Provider<List<String>>(
+  (ref) => throw UnimplementedError(),
+);
+
+// A StreamProvider.family that fetches prices for configured coins
+final cryptoPriceProvider = StreamProvider.family.autoDispose<double, String>((
+  ref,
+  coinId,
+) {
+  final coinIds = ref.watch(
+    cryptoPriceConfigProvider,
+  ); // Get the configured coins
+  final periodicStream = Stream<void>.periodic(const Duration(minutes: 5));
+
+  print("[INFO] Creating price stream provider for asset $coinId");
 
   return periodicStream
-      .asyncMap((_) => _fetchBitcoinPrice())
-      .startWithFuture(_fetchBitcoinPrice()); // immediate fetch
+      .asyncMap((_) => _fetchCryptoPrices(coinIds))
+      .startWithFuture(_fetchCryptoPrices(coinIds))
+      .map((prices) => prices[coinId] ?? 0.0); // Extract specific coin price
 });
 
-Future<double> _fetchBitcoinPrice() async {
+// Fetches prices for a list of coins in BRL
+Future<Map<String, double>> _fetchCryptoPrices(List<String> coinIds) async {
   final url = Uri.parse(
-    "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=brl",
+    "https://api.coingecko.com/api/v3/simple/price?ids=${coinIds.join(',')}&vs_currencies=brl",
   );
   final response = await http.get(url);
+
   if (response.statusCode == 200) {
+    print("[DEBUG] Retrieved price data.");
     final data = jsonDecode(response.body);
-    final brlPrice = data["bitcoin"]["brl"] as num;
-    return brlPrice.toDouble();
+    print(data.toString());
+    return Map.fromEntries(
+      coinIds.map(
+        (id) => MapEntry(id, (data[id]['brl'] as num?)?.toDouble() ?? 0.0),
+      ),
+    );
   } else {
     throw Exception(
-      "Erro ao buscar preço do Bitcoin: ${response.reasonPhrase}",
+      "Erro ao buscar preços das criptomoedas: ${response.reasonPhrase}",
     );
   }
 }
@@ -31,7 +52,7 @@ Future<double> _fetchBitcoinPrice() async {
 /// Extension method on Stream to prepend a Future's result
 extension StartWithFuture<T> on Stream<T> {
   Stream<T> startWithFuture(Future<T> future) async* {
-    yield await future; // emit once immediately
-    yield* this; // then continue with periodic stream
+    yield await future; // Emit once immediately
+    yield* this; // Then continue with periodic stream
   }
 }
