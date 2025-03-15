0c95026fcbbc54aaffd81a9958910bdd546f35df
h4vismat <h4vismat@pm.me>
2025-03-15 00:34:58 -0300

    Added support for actions in Appbar.

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class MoozeAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
+  final IconButton? action;
 
-  MoozeAppBar({required this.title, super.key});
+  MoozeAppBar({required this.title, this.action, super.key});
 
   @override
   PreferredSizeWidget build(BuildContext context) {
+    if (action != null) {
+      return AppBar(
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontFamily: "roboto",
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [action!],
+      );
+    }
+
     return AppBar(
       title: Text(
         title,
