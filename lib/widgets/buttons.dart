e9f40af1483778c0dff209b0a20b2b3dd1fa9d80
h4vismat <h4vismat@pm.me>
2025-03-15 00:35:21 -0300

    New disabled button.

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -16,13 +16,13 @@ class PrimaryButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ButtonStyle style = ElevatedButton.styleFrom(
-      foregroundColor: Colors.white,
-      backgroundColor: Colors.pinkAccent,
+      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+      backgroundColor: Theme.of(context).colorScheme.primary,
       textStyle: GoogleFonts.roboto(
         fontSize: 19.0,
         fontStyle: FontStyle.normal,
         fontWeight: FontWeight.w500,
-        color: Colors.white,
+        color: Theme.of(context).colorScheme.onPrimary,
         letterSpacing: 0.0,
       ),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
@@ -92,5 +92,42 @@ class TertiaryButton extends StatelessWidget {
       onPressed: onPressed,
       child: Text(text),
     );
+  }
+}
+
+class DisabledButton extends StatelessWidget {
+  final String text;
+  final IconData? icon;
+
+  const DisabledButton({required this.text, this.icon, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      foregroundColor: Colors.black,
+      backgroundColor: Colors.white,
+      textStyle: GoogleFonts.roboto(
+        fontSize: 19.0,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+        letterSpacing: 0.0,
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+      fixedSize: Size(345.0, 50.0),
+      iconAlignment: IconAlignment.start,
+      elevation: 3.0,
+    );
+
+    if (icon != null) {
+      return ElevatedButton.icon(
+        style: style,
+        icon: Icon(icon, color: Colors.black, size: 20.0),
+        onPressed: null,
+        label: Text(text),
+      );
+    }
+
+    return ElevatedButton(style: style, onPressed: null, child: Text(text));
   }
 }
