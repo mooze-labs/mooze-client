2e3b9308baa5320d0663c49d0afaadbc58e407b7
h4vismat <h4vismat@pm.me>
2025-03-18 00:59:45 -0300

    Add services and screens for PIN-based authentication.

---
+++ lib/screens/pin/create_pin.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:mooze_mobile/screens/pin/confirm_pin.dart';
+import 'package:mooze_mobile/screens/wallet/wallet.dart';
+import 'package:mooze_mobile/widgets/buttons.dart';
+import 'package:pinput/pinput.dart';
+
+import 'package:mooze_mobile/services/auth.dart';
+import 'package:mooze_mobile/widgets/appbar.dart';
+
+class CreatePinScreen extends StatefulWidget {
+  @override
+  State<CreatePinScreen> createState() => _CreatePinScreenState();
+}
+
+class _CreatePinScreenState extends State<CreatePinScreen> {
+  final AuthenticationService authenticationService = AuthenticationService();
+  final TextEditingController pinController = TextEditingController();
+
+  void onPinConfirmed(BuildContext context) async {
+    await authenticationService.createPin(pinController.text);
+    if (context.mounted) {
+      Navigator.of(context).popAndPushNamed("/wallet");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MoozeAppBar(title: "Criar PIN"),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Flex(
+          direction: Axis.vertical,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            Text(
+              "Digite seu PIN:",
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: "roboto",
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Pinput(
+              keyboardType: TextInputType.number,
+              length: 6,
+              onCompleted: (pin) => print(pinController.text),
+              controller: pinController,
+              defaultPinTheme: PinTheme(
+                width: 56,
+                height: 56,
+                textStyle: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).colorScheme.onSecondary,
+                  fontFamily: "roboto",
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "O PIN será utilizado para autorizar transações e acessar sua carteira.",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.onSurface,
+                fontFamily: "roboto",
+              ),
+            ),
+            Spacer(),
+            PrimaryButton(
+              text: "Continuar",
+              onPressed:
+                  () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) =>
+                              ConfirmPinScreen(pin: pinController.text),
+                    ),
+                  ),
+            ),
+            SizedBox(height: 100),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/auth.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:encrypt/encrypt.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:mooze_mobile/utils/mnemonic.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthenticationService {
+  static const int maxPinAttemps = 5;
+  static const int sessionTimeoutMinutes = 5;
+
+  final secureStorage = FlutterSecureStorage();
+
+  Future<bool> isPinSetup() async {
+    var hashedPin = await secureStorage.read(key: "hashedPin");
+    return hashedPin != null;
+  }
+
+  Future<bool> createPin(String pin) async {
+    if (pin.length < 4) {
+      return false;
+    }
+
+    final String salt = _generateSalt();
+
+    final bytes = utf8.encode("$pin$salt");
+    final digest = sha256.convert(bytes);
+
+    await secureStorage.write(key: "pinSalt", value: salt);
+    await secureStorage.write(key: "hashedPin", value: digest.toString());
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt("pinAttempts", 0);
+
+    return true;
+  }
+
+  Future<bool> authenticate(String pin) async {
+    var hashedPin = await secureStorage.read(key: "hashedPin");
+    if (hashedPin == null) {
+      throw Exception('No pin set');
+    }
+
+    var salt = await secureStorage.read(key: "pinSalt");
+    if (salt == null) {
+      throw Exception('No salt set');
+    }
+
+    var bytes = utf8.encode("$pin$salt");
+    var digest = sha256.convert(bytes);
+
+    bool success = digest.toString() == hashedPin;
+
+    await _updateAttempts(success);
+    return success;
+  }
+
+  Future<bool> hasValidSession() async {
+    final prefs = await SharedPreferences.getInstance();
+    final lastAuthTime = prefs.getInt("lastAuthTime");
+
+    if (lastAuthTime == null) {
+      return false;
+    }
+
+    final lastAuth = DateTime.fromMillisecondsSinceEpoch(lastAuthTime);
+    final diff = DateTime.now().difference(lastAuth);
+
+    return diff.inMinutes < sessionTimeoutMinutes;
+  }
+
+  Future<int> getAttempts() async {
+    final prefs = await SharedPreferences.getInstance();
+    var attempts = prefs.getInt("pinAttempts") ?? 0;
+    return attempts;
+  }
+
+  Future<bool> _tooManyAttempts() async {
+    final prefs = await SharedPreferences.getInstance();
+    var attempts = prefs.getInt("pinAttempts") ?? 0;
+    if (attempts >= maxPinAttemps) {
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<void> _updateAttempts(bool success) async {
+    final prefs = await SharedPreferences.getInstance();
+    var attempts = prefs.getInt("pinAttempts") ?? 0;
+
+    if (!success) {
+      prefs.setInt("pinAttemps", attempts++);
+    }
+
+    final exceededAttempts = await _tooManyAttempts();
+    if (exceededAttempts) {
+      await secureStorage.deleteAll(); // deletes EVERYTHING
+    }
+
+    await prefs.setInt("pinAttempts", 0);
+  }
+
+  Future<void> _updateLastAuthTime() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt("lastAuthTime", DateTime.now().millisecondsSinceEpoch);
+  }
+
+  String _generateSalt() {
+    final secureRandom = SecureRandom(16);
+    return base64Encode(secureRandom.bytes);
+  }
+}
