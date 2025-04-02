2c6916678022bc29052282de980400b5528d0dcf
h4vismat <h4vismat@pm.me>
2025-04-02 14:54:46 -0300

    Add terms and conditions.

--- lib/screens/first_access/first_access.dart
+++ lib/screens/first_access/first_access.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:mooze_mobile/screens/settings/terms_and_conditions.dart';
 import 'package:mooze_mobile/widgets/buttons.dart';
 
 class FirstAccessScreen extends StatelessWidget {
   const FirstAccessScreen({super.key});
+
+  void _importWallet(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Row(
+              children: [
+                Icon(Icons.warning, color: Colors.red),
+                SizedBox(width: 8),
+                Text("Aviso para usuários\nAqua/Sideswap"),
+              ],
+            ),
+            content: Text("""
+A carteira Mooze utiliza versões mais atualizadas da rede Liquid, com suporte para native segwit. As carteiras Sideswap e Aqua Wallet
+não possuem suporte para native segwit, portanto, não é possível importar seeds dessas carteiras para a Mooze. É recomendado que você
+crie uma nova carteira no nosso app e transfira os seus fundos para cá.
+          """),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pushNamed(context, "/import_wallet"),
+                child: Text("OK"),
+              ),
+            ],
+          ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,16 +40,6 @@ class FirstAccessScreen extends StatelessWidget {
 
     final buttons = Column(
       children: [
-        /*
-        PrimaryButton(
-          text: "Negociar agora",
-          onPressed: () {
-            Navigator.pushNamed(context, '/create_wallet');
-          },
-          icon: Icons.swap_horiz,
-        ),
-        SizedBox(height: 20),
-        */
         PrimaryButton(
           text: "Começar",
           onPressed: () {
@@ -35,7 +53,7 @@ class FirstAccessScreen extends StatelessWidget {
           children: [
             Text("Já tem uma carteira? ", style: defaultStyle),
             InkWell(
-              onTap: () => Navigator.pushNamed(context, "/import_wallet"),
+              onTap: () => _importWallet(context),
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -54,12 +72,57 @@ class FirstAccessScreen extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Spacer(),
           Image.asset('assets/images/mooze-logo.png', width: 200, height: 200),
           SizedBox(height: 50),
           SizedBox(
             width: MediaQuery.of(context).size.width * 0.8,
             child: Column(children: [buttons]),
           ),
+          Spacer(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Ao prosseguir, você concorda",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontSize: 14,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "com nossos",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontSize: 14,
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  InkWell(
+                    onTap:
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TermsAndConditionsScreen(),
+                          ),
+                        ),
+                    child: Text(
+                      "Termos e Condições",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 80),
         ],
       ),
     );
