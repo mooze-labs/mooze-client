9c98f19c1c5974e402816b4023befff7cd06696a
h4vismat <h4vismat@pm.me>
2025-04-28 14:33:28 -0300

    Change displayed QR code for PIX payments.

--- lib/screens/receive_pix/widgets/pix_qr_display.dart
+++ lib/screens/receive_pix/widgets/pix_qr_display.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class PixQrDisplay extends StatelessWidget {
   final String qrImageUrl;
@@ -17,8 +18,26 @@ class PixQrDisplay extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Image.network(qrImageUrl, width: 250.0, height: 250.0),
-        SizedBox(height: 10),
+        Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(color: Colors.transparent),
+          ),
+          child: QrImageView(
+            data: qrCopyPaste,
+            size: 250.0,
+            backgroundColor: Colors.white,
+            embeddedImage: AssetImage('assets/images/2.png'),
+            embeddedImageStyle: QrEmbeddedImageStyle(size: Size(60, 60)),
+            eyeStyle: QrEyeStyle(
+              color: Colors.black,
+              eyeShape: QrEyeShape.circle,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
           decoration: BoxDecoration(
