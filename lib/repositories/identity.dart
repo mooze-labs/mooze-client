003a98a364c09c2dae2421f4a42de14699caf9ef
h4vismat <h4vismat@pm.me>
2025-06-17 00:09:59 -0300

    Move to Nostr-based identity.

--- lib/repositories/identity.dart
+++ lib/repositories/identity.dart
@@ -1,59 +1,36 @@
-import 'package:cryptography/cryptography.dart';
-import 'package:blockchain_utils/blockchain_utils.dart';
-import 'package:hex/hex.dart';
+import 'package:dart_nostr/dart_nostr.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class IdentityRepository {
-  final List<int> _publicKey;
+class NostrKeyStore {
+  static const String _privKeyStorageKey = "nostr_priv_key";
+  static const String _pubKeyStorageKey = "nostr_pub_key";
 
-  IdentityRepository({required List<int> publicKey}) : _publicKey = publicKey;
-
-  List<int> get publicKey => _publicKey;
-  String get mpub => Bech32Encoder.encode("mpub", _publicKey);
-
-  static Future<IdentityRepository> fromSeed(List<int> seed) async {
-    final algorithm = Ed25519();
-    final keyPair = await algorithm.newKeyPairFromSeed(seed);
-    final pubKey = await keyPair.extractPublicKey().then(
-      (pubKey) => pubKey.bytes,
-    );
-
-    return IdentityRepository(publicKey: pubKey);
-  }
-
-  static Future<IdentityRepository> fromStorage() async {
+  static Future<bool> hasKeyPair() async {
     final storage = await SharedPreferences.getInstance();
-
-    final pubKey = storage.getString("identity_pub_key");
-    if (pubKey == null) {
-      throw Exception("No public key found in storage");
-    }
-
-    return IdentityRepository(publicKey: HEX.decode(pubKey));
-  }
-}
-
-class IdentityKeyStore {
-  static const String _privKeyStorageKey = "identity_priv_key";
-  static const String _pubKeyStorageKey = "identity_pub_key";
-
-  static Future<void> generateNewKeyPair() async {
-    final algorithm = Ed25519();
-    final keyPair = await algorithm.newKeyPair();
-    final privKey = await keyPair.extractPrivateKeyBytes();
-    final pubKey = await keyPair.extractPublicKey().then(
-      (pubKey) => pubKey.bytes,
-    );
-
-    await saveKeyPair(pubKey, privKey);
+    return storage.containsKey(_pubKeyStorageKey);
   }
 
-  static Future<void> saveKeyPair(List<int> pubKey, List<int> privKey) async {
+  static void generateNewKeyPair() async {
+    final nostr = Nostr.instance;
+    final privKey = nostr.keysService.generatePrivateKey();
+
+    await saveKeyPair(privKey);
+  }
+
+  static Future<void> saveKeyPair(String privKey) async {
+    final nostr = Nostr.instance;
     final storage = FlutterSecureStorage();
     final prefs = await SharedPreferences.getInstance();
 
-    await storage.write(key: _privKeyStorageKey, value: HEX.encode(privKey));
-    await prefs.setString(_pubKeyStorageKey, HEX.encode(pubKey));
+    final pubKey = nostr.keysService.derivePublicKey(privateKey: privKey);
+
+    await storage.write(key: _privKeyStorageKey, value: privKey);
+    await prefs.setString(_pubKeyStorageKey, pubKey);
+  }
+
+  static Future<String?> getPublicKey() async {
+    final storage = await SharedPreferences.getInstance();
+    return storage.getString(_pubKeyStorageKey);
   }
 }
