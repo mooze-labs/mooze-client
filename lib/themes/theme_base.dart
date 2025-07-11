9d6375b2de23ac5884c8e79c376d26df56e789fe
havis <h4vismat@mooze.app>
2025-07-11 04:08:04 -0300

    Update theme.

--- lib/themes/theme_base.dart
+++ lib/themes/theme_base.dart
@@ -9,14 +9,14 @@ final ColorScheme colorScheme = ColorScheme(
   onSecondary: Colors.white60,
   tertiary: Colors.white,
   onTertiary: Colors.black,
-  surface: Color(0xFF14181B),
+  surface: Color.fromARGB(255, 15, 15, 15),
   onSurface: Colors.white,
   error: Colors.red,
   onError: Colors.white,
 );
 
 final AppBarTheme appBarTheme = AppBarTheme(
-  backgroundColor: Color(0xFF14181B),
+  backgroundColor: Color.fromARGB(255, 15, 15, 15),
   centerTitle: true,
   titleTextStyle: TextStyle(
     fontSize: 22,
@@ -28,7 +28,7 @@ final AppBarTheme appBarTheme = AppBarTheme(
 
 final InputDecorationTheme inputDecorationTheme = InputDecorationTheme(
   border: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
+    borderRadius: BorderRadius.circular(8),
     borderSide: BorderSide(color: Colors.transparent, width: 0.0),
   ),
   filled: true,
@@ -40,7 +40,7 @@ final BottomNavigationBarThemeData bottomNavigationBarTheme =
     BottomNavigationBarThemeData(
       selectedItemColor: Colors.pinkAccent,
       unselectedItemColor: Colors.white,
-      backgroundColor: Color(0XFF14181B),
+      backgroundColor: Color.fromARGB(255, 15, 15, 15),
       showSelectedLabels: true,
       showUnselectedLabels: true,
     );
@@ -72,11 +72,28 @@ final PinTheme pinTheme = PinTheme(
   ),
 );
 
+const double buttonTextSize = 18.0;
+final ElevatedButtonThemeData elevatedButtonThemeData = ElevatedButtonThemeData(
+  style: ElevatedButton.styleFrom(
+    backgroundColor: colorScheme.primary,
+    foregroundColor: colorScheme.onPrimary,
+    textStyle: TextStyle(
+      fontSize: buttonTextSize,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Inter",
+      color: colorScheme.onPrimary,
+      letterSpacing: 0.0,
+    ),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+  ),
+);
+
 final ThemeData themeData = ThemeData(
   colorScheme: colorScheme,
   appBarTheme: appBarTheme,
   bottomNavigationBarTheme: bottomNavigationBarTheme,
   inputDecorationTheme: inputDecorationTheme,
   dropdownMenuTheme: dropdownMenuTheme,
-  fontFamily: "roboto",
+  elevatedButtonTheme: elevatedButtonThemeData,
+  fontFamily: "Inter",
 );
