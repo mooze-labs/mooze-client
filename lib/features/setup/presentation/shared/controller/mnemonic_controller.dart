284caebfbf213a813a9d8dea42eb73b3c87a354b
havis <h4vismat@mooze.app>
2025-07-10 21:36:43 -0300

    Move mnemonic repo to shared.

--- lib/features/setup/presentation/shared/controller/mnemonic_controller.dart
+++ lib/features/setup/presentation/shared/controller/mnemonic_controller.dart
@@ -1,10 +1,10 @@
 import 'package:fpdart/fpdart.dart';
-import 'package:mooze_mobile/core/repositories/mnemonic_repository.dart';
+import 'package:mooze_mobile/shared/key_management/mnemonic_store.dart';
 
 class MnemonicController {
-  final MnemonicRepository _repository;
+  final MnemonicStore _repository;
 
-  MnemonicController({required MnemonicRepository repository})
+  MnemonicController({required MnemonicStore repository})
     : _repository = repository;
 
   TaskEither<String, Option<String>> getMnemonic() {
--- lib/features/setup/presentation/shared/providers/mnemonic_controller_provider.dart
+++ lib/features/setup/presentation/shared/providers/mnemonic_controller_provider.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mooze_mobile/core/providers/mnemonic_repository_provider.dart';
+import 'package:mooze_mobile/shared/providers/mnemonic_store_provider.dart';
 
 import '../controller/mnemonic_controller.dart';
 
 final mnemonicControllerProvider = Provider<MnemonicController>((ref) {
-  final repository = ref.read(mnemonicRepositoryProvider);
+  final repository = ref.read(mnemonicStoreProvider);
   return MnemonicController(repository: repository);
 });
