511d6a7279e4c2ac8bf4b7b2272e866b1f164ee0
havis <h4vismat@mooze.app>
2025-07-14 23:44:29 -0300

    Add DI layer.

--- lib/features/setup/di/providers/mnemonic_repository_provider.dart
+++ lib/features/setup/di/providers/mnemonic_repository_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:mooze_mobile/shared/providers/mnemonic_store_provider.dart';
 
-import '../repositories/mnemonic_repository.dart';
+import '../../domain/repositories/mnemonic_repository.dart';
 import '../../data/repositories/mnemonic_repository_impl.dart';
 
 final mnemonicRepositoryProvider = Provider<MnemonicRepository>((ref) {
