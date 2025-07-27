3bc3d3871f1af0773bfb84a2ced532c90a40cd83
havis <h4vismat@mooze.app>
2025-07-27 18:00:51 -0300

    Move LWK data source to shared/

--- lib/shared/infra/lwk/wallet.dart
+++ lib/shared/infra/lwk/wallet.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:lwk/lwk.dart';
-import 'package:mooze_mobile/shared/entities/asset.dart';
-import 'package:mooze_mobile/features/swap/data/models/swap_utxo.dart';
 
 const String mnemonicKey = 'mnemonic';
 
@@ -23,36 +21,6 @@ class LiquidDataSource {
       electrumUrl: electrumUrl,
       validateDomain: validateDomain,
     );
-  }
-
-  Future<List<SwapUtxo>> getUtxos(Asset asset, BigInt amount) async {
-    final utxos = await wallet.utxos();
-    final selectedUtxos = <SwapUtxo>[];
-
-    BigInt remainingAmount = amount;
-
-    for (final utxo in utxos) {
-      if (utxo.unblinded.asset == Asset.toId(asset)) {
-        selectedUtxos.add(
-          SwapUtxo(
-            txid: utxo.outpoint.txid,
-            vout: utxo.outpoint.vout,
-            asset: utxo.unblinded.asset,
-            assetBf: utxo.unblinded.assetBf,
-            value: utxo.unblinded.value,
-            valueBf: utxo.unblinded.valueBf,
-          ),
-        );
-        remainingAmount -= utxo.unblinded.value;
-
-        // Early termination when we have enough funds
-        if (remainingAmount <= BigInt.zero) {
-          break;
-        }
-      }
-    }
-
-    return selectedUtxos;
   }
 
   Future<String> getAddress() async {
