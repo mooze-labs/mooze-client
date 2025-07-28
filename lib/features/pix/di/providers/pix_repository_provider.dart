9c9197f1b215319f06fdbfd6d00a2bb52d3c7ddb
havis <h4vismat@mooze.app>
2025-07-28 01:21:04 -0300

    Implement repository providers.

---
+++ lib/features/pix/di/providers/pix_repository_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:mooze_mobile/features/pix/data/datasources/pix_deposit_db.dart';
+
+import 'package:mooze_mobile/features/pix/domain/repositories.dart';
+import 'package:mooze_mobile/features/pix/data/repositories/pix_repository_impl.dart';
+import 'package:mooze_mobile/shared/infra/db/providers.dart';
+
+import 'package:mooze_mobile/shared/network/providers.dart';
+
+final pixRepositoryProvider = Provider<PixRepository>((ref) {
+  final authenticatedDioClient = ref.read(authenticatedClientProvider);
+  final pixDatabase = PixDepositDatabase(ref.read(appDatabaseProvider));
+
+  return PixRepositoryImpl(authenticatedDioClient, pixDatabase);
+});
