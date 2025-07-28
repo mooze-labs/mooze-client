5b0f9d4cb99f79c2c88b7ffdfb7f8adb5f830655
havis <h4vismat@mooze.app>
2025-07-28 04:50:57 -0300

    Add shimmer info row.

--- lib/shared/widgets/info_row.dart
+++ lib/shared/widgets/info_row.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class InfoRow extends StatelessWidget {
   final String label;
@@ -43,6 +44,56 @@ class InfoRow extends StatelessWidget {
             fontWeight: valueFontWeight ?? FontWeight.w500,
           ),
           textAlign: TextAlign.end,
+        ),
+      ],
+    );
+  }
+}
+
+class ShimmerInfoRow extends StatelessWidget {
+  final String label;
+  final Color? labelColor;
+  final FontWeight? labelFontWeight;
+  final double? fontSize;
+  final double? shimmerWidth;
+  final double? shimmerHeight;
+
+  const ShimmerInfoRow({
+    Key? key,
+    required this.label,
+    this.labelColor,
+    this.labelFontWeight,
+    this.fontSize = 14,
+    this.shimmerWidth = 80,
+    this.shimmerHeight = 16,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: labelColor ?? Colors.white70,
+            fontSize: fontSize,
+            fontWeight: labelFontWeight ?? FontWeight.normal,
+          ),
+        ),
+        SizedBox(width: 8),
+        Shimmer.fromColors(
+          baseColor: Colors.grey[800]!,
+          highlightColor: Colors.grey[600]!,
+          child: Container(
+            width: shimmerWidth,
+            height: shimmerHeight,
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
         ),
       ],
     );
