f1cf819c793ce59aa37d3094a68d9b1999772d1a
havis <h4vismat@mooze.app>
2025-07-28 04:44:57 -0300

    Move providers to shared folder.

---
+++ lib/features/pix/presentation/providers.dart
@@ -0,0 +1,7 @@
+export 'providers/deposit_amount_provider.dart';
+export 'providers/selected_asset_provider.dart';
+export 'providers/asset_quote_provider.dart';
+export 'providers/fee_rate_provider.dart';
+export 'providers/referral_provider.dart';
+export 'providers/payment_details_provider.dart';
+export 'providers/pix_deposit_controller_provider.dart';
--- lib/features/pix/presentation/screens/receive/providers.dart
+++ lib/features/pix/presentation/screens/receive/providers.dart
@@ -1,6 +1,2 @@
 export 'providers/amount_limit_provider.dart';
-export 'providers/asset_quote_provider.dart';
-export 'providers/deposit_amount_provider.dart';
-export 'providers/fee_rate_provider.dart';
-export 'providers/referral_provider.dart';
-export 'providers/selected_asset_provider.dart';
+export '../../providers.dart';
