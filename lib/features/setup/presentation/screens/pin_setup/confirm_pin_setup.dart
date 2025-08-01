dbfb547ab2a74a7da44e6c78f1c4f40a99f27eab
ogorek21 <ogorek@mooze.app>
2025-08-01 03:34:56 -0300

    Redesign and implement new PIN setup screen

--- lib/features/setup/presentation/screens/pin_setup/confirm_pin_setup.dart
+++ lib/features/setup/presentation/screens/pin_setup/confirm_pin_setup.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mooze_mobile/shared/widgets/buttons/primary_button.dart';
+import 'package:mooze_mobile/themes/pin_theme.dart';
 import 'package:pinput/pinput.dart';
 
 import '../../../di/providers/pin_setup_repository_provider.dart';
 
 class ConfirmPinSetupScreen extends ConsumerStatefulWidget {
   const ConfirmPinSetupScreen({super.key, required this.pin});
-
   final String pin;
 
   @override
@@ -17,87 +17,112 @@ class ConfirmPinSetupScreen extends ConsumerStatefulWidget {
 }
 
 class _ConfirmPinSetupScreenState extends ConsumerState<ConfirmPinSetupScreen> {
-  final TextEditingController pinController = TextEditingController();
+  final TextEditingController _pinController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+  bool isPinValid = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _pinController.addListener(() {
+      setState(() {
+        isPinValid = _pinController.text.length == 6;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pinController.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _onConfirmPressed() async {
+    final inputPin = _pinController.text;
+
+    if (inputPin != widget.pin) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("PINs não coincidem")));
+      return;
+    }
+
+    final pinSetupRepository = ref.read(pinSetupRepositoryProvider);
+    final result = await pinSetupRepository.createPin(inputPin).run();
+
+    result.match(
+      (failure) => ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(failure.toString()))),
+      (_) => context.go("/splash"),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final pinSetupRepository = ref.read(pinSetupRepositoryProvider);
     return Scaffold(
-      appBar: AppBar(title: Text("Confirme seu PIN")),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Flex(
-          direction: Axis.vertical,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Spacer(),
-            Text(
-              "Confirme seu PIN",
-              style: TextStyle(fontSize: 20, fontFamily: "Inter"),
-            ),
-            Spacer(),
-            Pinput(
-              keyboardType: TextInputType.number,
-              length: 6,
-              obscureText: true,
-              controller: pinController,
-              defaultPinTheme: PinTheme(
-                width: 56,
-                height: 56,
-                textStyle: TextStyle(
-                  fontSize: 20,
-                  fontFamily: "Inter",
-                  color: Theme.of(context).colorScheme.onSecondary,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-            ),
-            Spacer(),
-            ElevatedButton(
-              onPressed: () async {
-                if (pinController.text.length < 6) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text("PIN deve ter pelo menos 6 caracteres"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RichText(
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.headlineSmall,
+                  children: [
+                    const TextSpan(text: 'Confirme seu '),
+                    TextSpan(
+                      text: 'PIN',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
                     ),
-                  );
-                  return;
-                }
-
-                if (pinController.text != widget.pin) {
-                  ScaffoldMessenger.of(
-                    context,
-                  ).showSnackBar(SnackBar(content: Text("PINs não coincidem")));
-                  return;
-                }
-
-                final result =
-                    await pinSetupRepository
-                        .createPin(pinController.text)
-                        .run();
-
-                result.match(
-                  (failure) => ScaffoldMessenger.of(
-                    context,
-                  ).showSnackBar(SnackBar(content: Text(failure.toString()))),
-                  (_) => context.go("/splash"),
-                );
-              },
-              child: Text(
-                "Confirmar",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontFamily: "Inter",
-                  color: Theme.of(context).colorScheme.onPrimary,
+                  ],
                 ),
               ),
-            ),
-          ],
+
+              const SizedBox(height: 16),
+
+              RichText(
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  children: [
+                    const TextSpan(text: 'Digite novamente o '),
+                    TextSpan(
+                      text: 'PIN ',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const TextSpan(text: 'que você acabou de criar.'),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 50),
+
+              Pinput(
+                keyboardType: TextInputType.number,
+                length: 6,
+                obscureText: true,
+                controller: _pinController,
+                focusNode: _focusNode,
+                defaultPinTheme: PinThemes.focusedPinTheme,
+              ),
+
+              const SizedBox(height: 50),
+
+              PrimaryButton(
+                text: 'Confirmar',
+                onPressed: _onConfirmPressed,
+                isEnabled: isPinValid,
+              ),
+            ],
+          ),
         ),
       ),
     );
