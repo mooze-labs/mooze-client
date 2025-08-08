97f4a5b8b728c6ea61b2a7f3ea982113d3be2e59
havis <h4vismat@mooze.app>
2025-08-08 00:33:31 -0300

    Change default CURRENT_BUILD_VERSION.

--- lib/services/mooze/update.dart
+++ lib/services/mooze/update.dart
@@ -2,8 +2,14 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-const String versionDataUrl = String.fromEnvironment("UPDATE_DATA_URL", defaultValue: "mooze-public.s3.us-east-1.amazonaws.com");
-const String currentVersion = String.fromEnvironment("CURRENT_BUILD_VERSION", defaultValue: "1.2.2-beta");
+const String versionDataUrl = String.fromEnvironment(
+  "UPDATE_DATA_URL",
+  defaultValue: "mooze-public.s3.us-east-1.amazonaws.com",
+);
+const String currentVersion = String.fromEnvironment(
+  "CURRENT_BUILD_VERSION",
+  defaultValue: "1.2.3-beta",
+);
 
 class UpdateData {
   final String currentVersion;
@@ -11,9 +17,7 @@ class UpdateData {
   UpdateData({required this.currentVersion});
 
   factory UpdateData.fromJson(Map<String, dynamic> json) {
-    return UpdateData(
-        currentVersion: json["current_version"],
-    );
+    return UpdateData(currentVersion: json["current_version"]);
   }
 }
 
@@ -21,10 +25,14 @@ class UpdateService {
   final http.Client client = http.Client();
 
   Future<UpdateData> getUpdateData() async {
-    final response = await client.get(Uri.https(versionDataUrl, "/app_version.json"));
+    final response = await client.get(
+      Uri.https(versionDataUrl, "/app_version.json"),
+    );
 
     if (response.statusCode != 200) {
-      throw Exception("Failed to get update info. Status code: ${response.statusCode}");
+      throw Exception(
+        "Failed to get update info. Status code: ${response.statusCode}",
+      );
     }
 
     final Map<String, dynamic> data = jsonDecode(response.body);
@@ -32,3 +40,4 @@ class UpdateService {
     return UpdateData.fromJson(data);
   }
 }
+
