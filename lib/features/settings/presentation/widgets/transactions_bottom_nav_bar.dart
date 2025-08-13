e79594da7e3c5d6f4e7ee49a4e2b4a60e8fe8e8a
ogorek21 <ogorek@mooze.app>
2025-08-13 04:55:38 -0300

    Add TransactionsBottomNavBar

---
+++ lib/features/settings/presentation/widgets/transactions_bottom_nav_bar.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mooze_mobile/themes/app_colors.dart';
+
+class TransactionsBottomNavBar extends StatefulWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const TransactionsBottomNavBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  State<TransactionsBottomNavBar> createState() =>
+      _TransactionsBottomNavBarState();
+}
+
+class _TransactionsBottomNavBarState extends State<TransactionsBottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(
+              color: AppColors.backgroundCard,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                topLeft: Radius.circular(20),
+              ),
+            ),
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildNavItem(
+                  icon:
+                      'assets/new_ui_wallet/assets/icons/menu/navigation/asset.svg',
+                  index: 0,
+                  label: 'Transações',
+                ),
+                _buildNavItem(
+                  icon:
+                      'assets/new_ui_wallet/assets/icons/menu/navigation/swap.svg',
+                  index: 1,
+                  label: 'Swaps',
+                ),
+                _buildNavItem(
+                  icon:
+                      'assets/new_ui_wallet/assets/icons/menu/navigation/peg.svg',
+                  index: 2,
+                  label: 'Pegs',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNavItem({
+    required String icon,
+    required int index,
+    required String label,
+  }) {
+    bool isSelected = widget.currentIndex == index;
+
+    return GestureDetector(
+      onTap: () => widget.onTap(index),
+      child: SizedBox(
+        width: 70,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SvgPicture.asset(
+              icon,
+              width: 25,
+              height: 25,
+              color: isSelected ? AppColors.primaryColor : null,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              label,
+              style: TextStyle(
+                color:
+                    isSelected
+                        ? AppColors.primaryColor
+                        : AppColors.primaryIconColor,
+                fontSize: 11,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
