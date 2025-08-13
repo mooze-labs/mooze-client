88adabc78f770463cafbb869f3c50adb514390fb
ogorek21 <ogorek@mooze.app>
2025-08-13 04:49:36 -0300

    Add execute methods: action

---
+++ lib/features/settings/presentation/actions/action.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class SettingsActions {
+  void execute();
+}
+
+abstract class Actions extends StatelessWidget{
+  const Actions({super.key});
+}
