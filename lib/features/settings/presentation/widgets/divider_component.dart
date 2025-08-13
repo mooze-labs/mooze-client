50d5f03721738d7167773fbee52e7423cdd5998c
ogorek21 <ogorek@mooze.app>
2025-08-13 05:00:54 -0300

    Add LabelDivider to settings

---
+++ lib/features/settings/presentation/widgets/divider_component.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LabelDivider extends StatelessWidget {
+  const LabelDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 50),
+      child: Divider(
+        height: 0.1,
+        thickness: 1,
+      ),
+    );
+  }
+}
+
+class LabelDividerSelected extends StatelessWidget {
+  const LabelDividerSelected({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Divider(height: 0.1, thickness: 0.1);
+  }
+}
