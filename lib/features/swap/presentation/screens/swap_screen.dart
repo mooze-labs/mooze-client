982c99fb5cf3daf391292e8e6b49ff7c169fe865
ogorek21 <ogorek@mooze.app>
2025-08-21 05:43:06 -0300

    WIP SwapScreen

---
+++ lib/features/swap/presentation/screens/swap_screen.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mooze_mobile/features/swap/presentation/screens/confirm_swap_screen.dart';
+import 'package:mooze_mobile/shared/widgets.dart';
+import 'package:mooze_mobile/shared/widgets/buttons/text_button.dart';
+import 'package:mooze_mobile/themes/app_colors.dart';
+
+class SwapScreen extends StatefulWidget {
+  const SwapScreen({super.key});
+
+  @override
+  State<SwapScreen> createState() => _SwapScreenState();
+}
+
+class _SwapScreenState extends State<SwapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      appBar: AppBar(
+        title: const Text('Swap'),
+        leading: Icon(Icons.arrow_back_ios_new_rounded),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: AppColors.surfaceColor,
+          ),
+          width: double.infinity,
+          child: Column(
+            children: [
+              to(),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: SvgPicture.asset(
+                    'assets/new_ui_wallet/assets/icons/menu/swap.svg',
+                  ),
+                ),
+              ),
+              from(),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '1 L-BTC = 1 BTC (112,023.80)',
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                  Text(
+                    'Fee: 2321 SATS',
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Center(child: Text('Powered by sideswap.io')),
+              SizedBox(height: 15),
+              PrimaryButton(
+                text: 'swap',
+                onPressed: () {
+                  ConfirmSwapBottomSheet.show(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget to() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: AppColors.backgroundColor,
+      ),
+      height: 111,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Você envia', style: Theme.of(context).textTheme.labelLarge),
+              Row(
+                children: [
+                  Text('Balance: 1'),
+                  TransparentTextButton(
+                    text: 'MAX',
+                    onPressed: () {},
+                    style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                      color: AppColors.primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SvgPicture.asset(
+                'assets/new_ui_wallet/assets/icons/asset/depix.svg',
+                width: 30,
+                height: 30,
+              ),
+              SizedBox(width: 5),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Depix',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    Text(
+                      '10000',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text('bitcoin layer 2'), Text('~ 24,653.80')],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget from() {
+    return Container(
+      height: 111,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.centerRight,
+          end: Alignment.centerLeft,
+          colors: [Color(0xFF2D2E2A), AppColors.primaryColor],
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(1.5),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          decoration: BoxDecoration(
+            color: AppColors.surfaceColor,
+            borderRadius: BorderRadius.circular(13),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Você Recebe',
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Balance: 1',
+                        style:
+                            Theme.of(context).textTheme.labelLarge!.copyWith(),
+                      ),
+                      TransparentTextButton(
+                        text: 'MAX',
+                        onPressed: () {
+                        },
+                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  SvgPicture.asset(
+                    'assets/new_ui_wallet/assets/icons/asset/bitcoin.svg',
+                    width: 30,
+                    height: 30,
+                  ),
+                  SizedBox(width: 5),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'BTC',
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
+                        Text(
+                          '0.1912',
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text('Depix'), Text('~ 24,653.80')],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
