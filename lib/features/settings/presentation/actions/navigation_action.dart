052ca80f0969a553d09cbd5ef2c063c840ab6c09
ogorek21 <ogorek@mooze.app>
2025-08-21 05:36:07 -0300

    Add verifyPinArgs in Navigation

--- lib/features/settings/presentation/actions/navigation_action.dart
+++ lib/features/settings/presentation/actions/navigation_action.dart
@@ -5,14 +5,32 @@ import 'package:mooze_mobile/features/settings/presentation/actions/action.dart'
 class Navigation extends SettingsActions {
   final String rota;
   final BuildContext context;
+  final VerifyPinArgs? verifyPinArgs;
 
   Navigation({
     required this.rota,
     required this.context,
+    this.verifyPinArgs,
   });
 
   @override
   void execute() {
-    context.go(rota);
+    context.push(
+      rota,
+      extra: verifyPinArgs,
+    );
   }
+}
+
+
+class VerifyPinArgs {
+  final Function()? onPinConfirmed;
+  final bool? forceAuth;
+  final bool? isAppResuming;
+
+  VerifyPinArgs({
+    this.onPinConfirmed,
+    this.forceAuth,
+    this.isAppResuming,
+  });
 }
