77fbf0aa465d421e6ca0fc0a18c8bd215d2d21af
ogorek21 <ogorek@mooze.app>
2025-08-21 05:35:47 -0300

    Implement PrimaryButton in ImportWallet

--- lib/features/setup/presentation/screens/import_wallet/widgets/import_button.dart
+++ lib/features/setup/presentation/screens/import_wallet/widgets/import_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mooze_mobile/shared/widgets/buttons/primary_button.dart';
 
 import '../providers/mnemonic_input_provider.dart';
 import '../../../providers/mnemonic_controller_provider.dart';
@@ -12,24 +13,18 @@ class ImportButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final mnemonicInput = ref.watch(mnemonicInputProvider);
     final mnemonicController = ref.watch(mnemonicControllerProvider);
-
-    return ElevatedButton(
+    return PrimaryButton(
+      text: "Importar",
       onPressed: () async {
         final result =
             await mnemonicController.saveMnemonic(mnemonicInput).run();
-
         result.match(
           (failure) => ScaffoldMessenger.of(
             context,
           ).showSnackBar(SnackBar(content: Text(failure))),
-          (success) => context.go("/setup/pin/new"),
+          (success) => context.push("/setup/pin/new"),
         );
       },
-      style: ElevatedButton.styleFrom(minimumSize: Size(double.infinity, 48)),
-      child: Text(
-        "Importar",
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-      ),
     );
   }
 }
