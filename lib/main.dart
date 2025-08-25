5b7a59a30d9a7e1f9560741a38b63e135520c5cb
ogorek21 <ogorek@mooze.app>
2025-08-25 13:14:01 -0300

    Clean up main.dart by removing commented-out code and maintaining initialization order

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_breez_liquid/flutter_breez_liquid.dart' as liquidSdk;
 import 'package:mooze_mobile/themes/app_theme.dart';
-// import 'themes/theme_base.dart';
-
-// import 'package:mooze_mobile/services/notifications.dart';
-// import 'package:mooze_mobile/widgets/lifecycle_manager.dart';
 import 'routes.dart';
 
-// Create a global navigator key
-//final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
 void main() async {
-  // await LibLwk.init();
-  await liquidSdk.initialize();
   WidgetsFlutterBinding.ensureInitialized();
+  // await LibLwk.init();
 
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // final notificationService = NotificationService();
-  // await notificationService.initialize();
+  await liquidSdk.initialize();
 
   runApp(ProviderScope(child: MyApp()));
 }
@@ -31,7 +20,7 @@ class MyApp extends StatelessWidget {
       title: 'Mooze',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkTheme(context),
-      themeMode: ThemeMode.dark, 
+      themeMode: ThemeMode.dark,
       routerConfig: router,
     );
   }
