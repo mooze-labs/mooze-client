42d4fda4d50e0118c24749a31056da0eab97d8fa
ogorek21 <ogorek@mooze.app>
2025-08-26 17:47:36 -0300

    Refactor AssetSelectorWidget to use SVG icons for improved asset representation

--- lib/features/pix/presentation/screens/receive/widgets/asset_selector_widget.dart
+++ lib/features/pix/presentation/screens/receive/widgets/asset_selector_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:mooze_mobile/features/new_ui_wallet/shared/widgets/dropdownburron.dart';
 
 import 'package:mooze_mobile/shared/entities/asset.dart';
@@ -11,8 +12,8 @@ class AssetSelectorWidget extends ConsumerWidget {
   const AssetSelectorWidget({super.key});
 
   Widget _buildAssetIcon(Asset asset) {
-    return Image.asset(
-      'assets/images/logos/${asset.name.toLowerCase()}',
+    return SvgPicture.asset(
+      asset.iconPath,
       width: 16,
       height: 16,
       fit: BoxFit.contain,
@@ -32,7 +33,6 @@ class AssetSelectorWidget extends ConsumerWidget {
       itemIconBuilder: (asset) => _buildAssetIcon(asset),
       itemLabelBuilder: (asset) => asset.name,
       borderColor: Theme.of(context).colorScheme.primary,
-      backgroundColor: Theme.of(context).colorScheme.secondary,
     );
   }
 }
