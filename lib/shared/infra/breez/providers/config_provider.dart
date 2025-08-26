7b16afbb122ff2a460786a7af161f223bc28db1c
ogorek21 <ogorek@mooze.app>
2025-08-26 17:30:00 -0300

    use const instead of final to read --dart-define

--- lib/shared/infra/breez/providers/config_provider.dart
+++ lib/shared/infra/breez/providers/config_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter_breez_liquid/flutter_breez_liquid.dart';
 import 'package:path_provider/path_provider.dart';
 
 final configProvider = FutureProvider<Config>((ref) async {
-  final breezApiKey = String.fromEnvironment('BREEZ_API_KEY');
+  const breezApiKey = String.fromEnvironment('BREEZ_API_KEY');
   final workingDir = await getApplicationDocumentsDirectory();
   final network = switch (String.fromEnvironment(
     "BLOCKCHAIN_NETWORK_TYPE",
@@ -18,22 +18,24 @@ final configProvider = FutureProvider<Config>((ref) async {
   final defaultBreezConfig = defaultConfig(network: network);
 
   Config config = Config(
-      liquidExplorer: defaultBreezConfig.liquidExplorer,
-      bitcoinExplorer: defaultBreezConfig.bitcoinExplorer,
-      workingDir: "${workingDir.path}/mooze",
-      network: network,
-      paymentTimeoutSec: defaultBreezConfig.paymentTimeoutSec,
-      useDefaultExternalInputParsers: defaultBreezConfig.useDefaultExternalInputParsers,
-      breezApiKey: breezApiKey,
-      assetMetadata: [
-        AssetMetadata(
-            assetId: "02f22f8d9c76ab41661a2729e4752e2c5d1a263012141b86ea98af5472df5189",
-            name: "DePix",
-            ticker: "DEPIX",
-            precision: 8
-        )
-      ],
-      useMagicRoutingHints: true
+    liquidExplorer: defaultBreezConfig.liquidExplorer,
+    bitcoinExplorer: defaultBreezConfig.bitcoinExplorer,
+    workingDir: "${workingDir.path}/mooze",
+    network: network,
+    paymentTimeoutSec: defaultBreezConfig.paymentTimeoutSec,
+    useDefaultExternalInputParsers:
+        defaultBreezConfig.useDefaultExternalInputParsers,
+    breezApiKey: breezApiKey,
+    assetMetadata: [
+      AssetMetadata(
+        assetId:
+            "02f22f8d9c76ab41661a2729e4752e2c5d1a263012141b86ea98af5472df5189",
+        name: "DePix",
+        ticker: "DEPIX",
+        precision: 8,
+      ),
+    ],
+    useMagicRoutingHints: true,
   );
 
   return config;
