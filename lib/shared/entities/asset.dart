676148803b2340430804f784617267135a9f28a3
ogorek21 <ogorek@mooze.app>
2025-08-26 17:30:10 -0300

    Add iconPath getter to Asset enum for asset icon retrieval

--- lib/shared/entities/asset.dart
+++ lib/shared/entities/asset.dart
@@ -51,4 +51,12 @@ enum Asset {
       Asset.btc => lbtcAssetId,
     };
   }
+
+  String get iconPath {
+    return switch (this) {
+      Asset.usdt => 'assets/new_ui_wallet/assets/icons/asset/tether.svg',
+      Asset.depix => 'assets/new_ui_wallet/assets/icons/asset/depix.svg',
+      Asset.btc => 'assets/new_ui_wallet/assets/icons/asset/bitcoin.svg',
+    };
+  }
 }
