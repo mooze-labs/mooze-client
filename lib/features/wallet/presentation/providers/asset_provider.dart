c47c756e8428cdcbb43be1edabb519d3832a1f99
ogorek21 <ogorek@mooze.app>
2025-08-27 18:26:33 -0300

    feat(asset_provider): add providers for managing all and favorite assets

---
+++ lib/features/wallet/presentation/providers/asset_provider.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mooze_mobile/shared/entities/asset.dart';
+
+final allAssetsProvider = Provider<List<Asset>>((ref) {
+  return const [Asset.btc, Asset.usdt, Asset.depix];
+});
+
+final favoriteAssetsProvider = StateProvider<List<Asset>>((ref) {
+  return const [Asset.btc, Asset.usdt];
+});
+
+final isFavoriteAssetProvider = Provider.family<bool, Asset>((ref, asset) {
+  final favoriteAssets = ref.watch(favoriteAssetsProvider);
+  return favoriteAssets.contains(asset);
+});
+
+extension FavoriteAssetsNotifier on StateController<List<Asset>> {
+  void toggleFavorite(Asset asset) {
+    final currentFavorites = state;
+    if (currentFavorites.contains(asset)) {
+      state = currentFavorites.where((a) => a != asset).toList();
+    } else {
+      if (currentFavorites.length < 2) {
+        state = [...currentFavorites, asset];
+      } else {
+        state = [currentFavorites[1], asset];
+      }
+    }
+  }
+
+  void setFavorites(List<Asset> assets) {
+    // Limita a 2 ativos favoritos
+    state = assets.take(2).toList();
+  }
+}
