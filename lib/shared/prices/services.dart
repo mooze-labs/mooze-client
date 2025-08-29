0726b0742a678cefa0a6838b5a4a3cb09929c32d
ogorek21 <ogorek@mooze.app>
2025-08-28 22:56:22 -0300

    fix(services): add missing export for HybridPriceService

--- lib/shared/prices/services.dart
+++ lib/shared/prices/services.dart
@@ -2,3 +2,4 @@ export 'services/binance_price_service.dart';
 export 'services/coingecko_price_service_impl.dart';
 export 'services/mock_price_repository_impl.dart';
 export 'services/price_service.dart';
+export 'services/hybrid_price_service.dart';
