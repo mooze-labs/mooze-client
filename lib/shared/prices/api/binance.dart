d9a25680cc8adb25e145946967fe7b389f5d68b2
ogorek21 <ogorek@mooze.app>
2025-08-28 22:56:16 -0300

    refactor(binance_api): improve API structure and enhance readability

--- lib/shared/prices/api/binance.dart
+++ lib/shared/prices/api/binance.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
 
+const String binanceApiHost = "data-api.binance.vision";
+const String binanceApiPath = "/api/v3/";
 const String binanceApiUrl = "https://data-api.binance.vision/api/v3/";
 const String symbols = "[\"BTCBRL\",\"BTCUSDT\",\"USDTBRL\"]";
 
@@ -17,14 +19,17 @@ class BinancePriceCache {
   Map<String, DateTime> _klinesLastFetch = {};
   static const Duration _cacheDuration = Duration(seconds: 60);
 
-  bool get _shouldRefresh => _lastFetch == null || 
-    DateTime.now().difference(_lastFetch!) > _cacheDuration;
+  bool get _shouldRefresh =>
+      _lastFetch == null ||
+      DateTime.now().difference(_lastFetch!) > _cacheDuration;
 
-  bool _shouldRefreshKlines(String symbol) => 
-    !_klinesLastFetch.containsKey(symbol) || 
-    DateTime.now().difference(_klinesLastFetch[symbol]!) > _cacheDuration;
+  bool _shouldRefreshKlines(String symbol) =>
+      !_klinesLastFetch.containsKey(symbol) ||
+      DateTime.now().difference(_klinesLastFetch[symbol]!) > _cacheDuration;
 
-  TaskEither<String, List<Map<String, dynamic>>> getCachedPrices(BinanceApi api) {
+  TaskEither<String, List<Map<String, dynamic>>> getCachedPrices(
+    BinanceApi api,
+  ) {
     if (_shouldRefresh) {
       return api.fetchPrices().map((data) {
         _cachedData = data;
@@ -35,9 +40,15 @@ class BinancePriceCache {
     return TaskEither.right(_cachedData!);
   }
 
-  TaskEither<String, List<List<dynamic>>> getCachedKlines(BinanceApi api, String symbol, String interval, int startTime, int endTime) {
+  TaskEither<String, List<List<dynamic>>> getCachedKlines(
+    BinanceApi api,
+    String symbol,
+    String interval,
+    int startTime,
+    int endTime,
+  ) {
     final cacheKey = "${symbol}_${interval}_${startTime}_$endTime";
-    
+
     if (_shouldRefreshKlines(cacheKey)) {
       return api.fetchKlines(symbol, interval, startTime, endTime).map((data) {
         _cachedKlines[cacheKey] = data;
@@ -50,24 +61,33 @@ class BinancePriceCache {
 }
 
 class BinanceApi {
-  final String baseUrl = binanceApiUrl;
-
   BinanceApi();
 
   TaskEither<String, List<Map<String, dynamic>>> fetchPrices() {
-    final url = Uri.https(binanceApiUrl, "ticker/24hr", {"symbols": symbols});
+    final url = Uri.https(binanceApiHost, "${binanceApiPath}ticker/24hr", {
+      "symbols": symbols,
+    });
 
     return _get(url).flatMap((response) {
       if (response.statusCode != 200) {
-        return TaskEither.left("Failed to query Binance API: ${response.statusCode}");
+        return TaskEither.left(
+          "Failed to query Binance API: ${response.statusCode}",
+        );
       }
 
-      return jsonDecode(response.body);
+      return TaskEither.right(
+        List<Map<String, dynamic>>.from(jsonDecode(response.body)),
+      );
     });
   }
 
-  TaskEither<String, List<List<dynamic>>> fetchKlines(String symbol, String interval, int startTime, int endTime) {
-    final url = Uri.parse("${baseUrl}klines").replace(queryParameters: {
+  TaskEither<String, List<List<dynamic>>> fetchKlines(
+    String symbol,
+    String interval,
+    int startTime,
+    int endTime,
+  ) {
+    final url = Uri.https(binanceApiHost, "${binanceApiPath}klines", {
       "symbol": symbol,
       "interval": interval,
       "startTime": startTime.toString(),
@@ -76,16 +96,21 @@ class BinanceApi {
 
     return _get(url).flatMap((response) {
       if (response.statusCode != 200) {
-        return TaskEither.left("Failed to query Binance Klines API: ${response.statusCode}");
+        return TaskEither.left(
+          "Failed to query Binance Klines API: ${response.statusCode}",
+        );
       }
 
-      return TaskEither.right(List<List<dynamic>>.from(jsonDecode(response.body)));
+      return TaskEither.right(
+        List<List<dynamic>>.from(jsonDecode(response.body)),
+      );
     });
   }
-  
+
   TaskEither<String, http.Response> _get(Uri url) {
     return TaskEither.tryCatch(
       () async => await http.get(url),
-      (err, stackTrace) => err.toString());
+      (err, stackTrace) => err.toString(),
+    );
   }
 }
