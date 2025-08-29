4052132e0a09c3176110dfa0a6cdb32e3cb6ac4a
ogorek21 <ogorek@mooze.app>
2025-08-28 23:03:41 -0300

    style(balance_provider): format code for improved readability and consistency

--- lib/features/wallet/presentation/providers/balance_provider.dart
+++ lib/features/wallet/presentation/providers/balance_provider.dart
@@ -5,19 +5,21 @@ import 'package:mooze_mobile/features/wallet/domain/errors.dart';
 import 'package:mooze_mobile/features/wallet/presentation/controllers/balance_controller.dart';
 import 'package:mooze_mobile/shared/entities/asset.dart';
 
-final balanceControllerProvider = FutureProvider<Either<WalletError, BalanceController>>((ref) async {
-  final wallet = await ref.read(walletRepositoryProvider.future);
-  return wallet.flatMap((w) => Either.right(BalanceController(w)));
-});
+final balanceControllerProvider =
+    FutureProvider<Either<WalletError, BalanceController>>((ref) async {
+      final wallet = await ref.read(walletRepositoryProvider.future);
+      return wallet.flatMap((w) => Either.right(BalanceController(w)));
+    });
 
-final balanceProvider = FutureProvider.family<Either<WalletError, BigInt>, Asset>((
-  ref,
-  Asset asset,
-) async {
-  final controller = await ref.read(balanceControllerProvider.future);
+final balanceProvider =
+    FutureProvider.family<Either<WalletError, BigInt>, Asset>((
+      ref,
+      Asset asset,
+    ) async {
+      final controller = await ref.read(balanceControllerProvider.future);
 
-  return controller.fold(
-      (err) async => Either.left(err),
-      (controller) async => controller.getAssetBalance(asset).run()
-  );
-});
+      return controller.fold(
+        (err) async => Either.left(err),
+        (controller) async => controller.getAssetBalance(asset).run(),
+      );
+    });
