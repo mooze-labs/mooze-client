9bb40134bdeb0a6a986e13f878a3a8562a26da1d
ogorek21 <ogorek@mooze.app>
2025-09-01 18:36:20 -0300

    feat: Add selected asset price provider to fetch asset prices

--- lib/features/wallet/presentation/providers/send_funds/bitcoin_price_provider.dart
+++ lib/features/wallet/presentation/providers/send_funds/bitcoin_price_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mooze_mobile/shared/entities/asset.dart';
 import 'package:mooze_mobile/shared/prices/providers/price_service_provider.dart';
 import 'package:mooze_mobile/shared/prices/providers/currency_controller_provider.dart';
+import 'selected_asset_provider.dart';
 
 final bitcoinPriceProvider = FutureProvider<double>((ref) async {
   final priceServiceResult = await ref.read(priceServiceProvider).run();
@@ -18,6 +19,30 @@ final bitcoinPriceProvider = FutureProvider<double>((ref) async {
         },
         (priceOption) => priceOption.fold(() {
           throw Exception('Preço do Bitcoin não disponível');
+        }, (price) => price),
+      );
+    },
+  );
+});
+
+final selectedAssetPriceProvider = FutureProvider<double>((ref) async {
+  final selectedAsset = ref.watch(selectedAssetProvider);
+  final priceServiceResult = await ref.read(priceServiceProvider).run();
+
+  return await priceServiceResult.fold(
+    (error) async {
+      throw Exception('Serviço de preços indisponível: $error');
+    },
+    (service) async {
+      final assetPriceResult = await service.getCoinPrice(selectedAsset).run();
+      return assetPriceResult.fold(
+        (error) {
+          throw Exception(
+            'Erro ao obter cotação de ${selectedAsset.name}: $error',
+          );
+        },
+        (priceOption) => priceOption.fold(() {
+          throw Exception('Preço de ${selectedAsset.name} não disponível');
         }, (price) => price),
       );
     },
