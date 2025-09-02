07c6d4bac4f46d6a85ea32749f8aafd9f149aa9a
ogorek21 <ogorek@mooze.app>
2025-09-02 17:51:17 -0300

    fix: replace Container with SizedBox for better layout in CustomBottomNavBar

--- lib/shared/widgets/bottom_nav_bar/custom_bottom_nav_bar.dart
+++ lib/shared/widgets/bottom_nav_bar/custom_bottom_nav_bar.dart
@@ -30,7 +30,7 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
           bottom: 0,
           left: 0,
           right: 0,
-          child: Container(
+          child: SizedBox(
             height: 95,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
