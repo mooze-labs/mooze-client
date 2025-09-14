bc31cac93d49092a14b19b4a2a407219925c0575
ogorek21 <ogorek@mooze.app>
2025-09-14 18:52:22 -0300

    refactor: remove unused home_transaction_item export from widgets

--- lib/features/wallet/presentation/widgets/widgets.dart
+++ lib/features/wallet/presentation/widgets/widgets.dart
@@ -1,5 +1,4 @@
 export 'home/action_button.dart';
 export 'home/asset_graph_card.dart';
 export 'home/header.dart';
-export 'home/home_transaction_item.dart';
 export 'home/transaction_section.dart';
