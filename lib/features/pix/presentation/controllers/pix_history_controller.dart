ec6283b981894f678b1201aeb05f32a06ea23e15
havis <h4vismat@mooze.app>
2025-09-19 20:49:33 -0300

    Change controller to fetch Pix history while requesting updates for it.

--- lib/features/pix/presentation/controllers/pix_history_controller.dart
+++ lib/features/pix/presentation/controllers/pix_history_controller.dart
@@ -1,41 +1,39 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:mooze_mobile/features/pix/domain/entities/pix_deposit.dart';
+import 'package:mooze_mobile/features/pix/domain/repositories/pix_repository.dart';
 import 'package:mooze_mobile/features/pix/presentation/providers/pix_history_provider.dart';
 
-class PixHistoryController extends StateNotifier<AsyncValue<List<PixDeposit>>> {
-  final Ref _ref;
+class PixHistoryController {
+  final PixRepository _repo;
 
-  PixHistoryController(this._ref) : super(const AsyncValue.loading()) {
-    loadPixHistory();
+  PixHistoryController(PixRepository repo) : _repo = repo;
+
+  TaskEither<String, List<PixDeposit>> getPixHistory({
+    int? limit,
+    int? offset,
+  }) {
+    return _repo.getDeposits(limit: limit, offset: offset).flatMap((d) {
+      final pendingDeposits =
+          d
+              .filter(
+                (t) =>
+                    ((t.status != DepositStatus.finished) &&
+                        (t.status != DepositStatus.expired)),
+              )
+              .map((d) => d.depositId)
+              .toList();
+
+      return TaskEither.tryCatch(() async {
+        if (pendingDeposits.isNotEmpty) {
+          await _repo.updateDepositDetails(pendingDeposits).run();
+        }
+        return _repo.getDeposits(limit: limit, offset: offset);
+      }, (error, _) => error.toString()).flatMap((task) => task);
+    });
   }
 
-  Future<void> loadPixHistory() async {
-    state = const AsyncValue.loading();
-
-    final result = await _ref.read(pixDepositHistoryProvider.future);
-
-    result.fold(
-      (error) => state = AsyncValue.error(error, StackTrace.current),
-      (deposits) => state = AsyncValue.data(deposits),
-    );
-  }
-
-  Future<void> refreshPixHistory() async {
-    _ref.invalidate(pixDepositHistoryProvider);
-    await loadPixHistory();
-  }
-
-  Future<Either<String, PixDeposit?>> getPixDeposit(String depositId) async {
-    final result = await _ref.read(pixDepositProvider(depositId).future);
-
-    return result.map((option) => option.toNullable());
+  TaskEither<String, Option<PixDeposit>> getPixDeposit(String depositId) {
+    return _repo.getDeposit(depositId);
   }
 }
-
-final pixHistoryControllerProvider =
-    StateNotifierProvider<PixHistoryController, AsyncValue<List<PixDeposit>>>((
-      ref,
-    ) {
-      return PixHistoryController(ref);
-    });
