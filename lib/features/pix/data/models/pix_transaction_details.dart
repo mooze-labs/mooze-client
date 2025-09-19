971248e4523b10fa4bbe3c539c739eacff81bb9a
havis <h4vismat@mooze.app>
2025-09-19 20:46:58 -0300

    Add new transaction details model. Implemented new Pix repository implementation that queries the underlying Mooze API for transaction statuses.

---
+++ lib/features/pix/data/models/pix_transaction_details.dart
@@ -0,0 +1,25 @@
+class PixTransactionDetails {
+  final String id;
+  final String status;
+  final int amountInCents;
+  final String? blockchainTxid;
+  final int? assetAmount;
+
+  PixTransactionDetails({
+    required this.id,
+    required this.status,
+    required this.amountInCents,
+    this.blockchainTxid,
+    this.assetAmount,
+  });
+
+  factory PixTransactionDetails.fromJson(Map<String, dynamic> json) {
+    return PixTransactionDetails(
+      id: json['id'],
+      status: json['status'],
+      amountInCents: json['amount_in_cents'],
+      blockchainTxid: json['blockchain_txid'],
+      assetAmount: json['asset_amount'],
+    );
+  }
+}
