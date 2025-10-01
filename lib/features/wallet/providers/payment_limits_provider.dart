cdbc7990d7290ba4b18defb0ae3509404417ba3b
ogorek21 <ogorek@mooze.app>
2025-09-30 23:37:50 -0300

    refactor: remove onchain limits provider to simplify payment limits handling

--- lib/features/wallet/providers/payment_limits_provider.dart
+++ lib/features/wallet/providers/payment_limits_provider.dart
@@ -3,21 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../domain/entities/payment_limits.dart';
 import '../di/providers/wallet_repository_provider.dart';
 
-final onchainLimitsProvider = FutureProvider<OnchainPaymentLimitsResponse?>((
-  ref,
-) async {
-  final walletRepositoryEither = await ref.read(
-    walletRepositoryProvider.future,
-  );
-
-  return await walletRepositoryEither.fold((error) async => null, (
-    walletRepository,
-  ) async {
-    final result = await walletRepository.fetchOnchainLimits().run();
-    return result.fold((error) => null, (limits) => limits);
-  });
-});
-
 final lightningLimitsProvider = FutureProvider<LightningPaymentLimitsResponse?>(
   (ref) async {
     final walletRepositoryEither = await ref.read(
@@ -33,11 +18,6 @@ final lightningLimitsProvider = FutureProvider<LightningPaymentLimitsResponse?>(
   },
 );
 
-final onchainSendLimitsProvider = FutureProvider<PaymentLimits?>((ref) async {
-  final onchainLimits = await ref.watch(onchainLimitsProvider.future);
-  return onchainLimits?.send;
-});
-
 final lightningSendLimitsProvider = FutureProvider<PaymentLimits?>((ref) async {
   final lightningLimits = await ref.watch(lightningLimitsProvider.future);
   return lightningLimits?.send;
@@ -46,16 +26,10 @@ final lightningSendLimitsProvider = FutureProvider<PaymentLimits?>((ref) async {
 final paymentLimitsSummaryProvider = FutureProvider<PaymentLimits?>((
   ref,
 ) async {
-  final onchainLimits = await ref.watch(onchainLimitsProvider.future);
   final lightningLimits = await ref.watch(lightningLimitsProvider.future);
 
   BigInt? minSat;
   BigInt? maxSat;
-
-  if (onchainLimits != null) {
-    minSat = onchainLimits.send.minSat;
-    maxSat = onchainLimits.send.maxSat;
-  }
 
   if (lightningLimits != null) {
     if (minSat == null || lightningLimits.send.minSat < minSat) {
