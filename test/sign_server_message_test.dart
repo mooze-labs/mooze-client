226cab5cc69a091eb418b1a15a93ad373b8a7cc7
h4vismat <h4vismat@mooze.app>
2025-10-08 12:39:32 -0300

    Add test to sign server message.

---
+++ test/sign_server_message_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:mooze_mobile/shared/authentication/services.dart';
+import 'package:mooze_mobile/shared/authentication/clients/ecdsa_signature_client.dart';
+import 'package:mooze_mobile/shared/authentication/clients/signature_client.dart';
+import 'package:mooze_mobile/shared/authentication/services/remote_auth_service_impl.dart';
+
+const testMnemonic =
+    'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
+
+void main() {
+  group('RemoteAuthenticationClient tests', () {
+    late RemoteAuthenticationService remoteAuthenticationService;
+
+    setUp(() {
+      remoteAuthenticationService = RemoteAuthServiceImpl.withEcdsaClient(testMnemonic); 
+    });
+
+    test('should retrieve a challenge from the server', () async { 
+      final challengeResult = await remoteAuthenticationService.requestLoginChallenge().run();
+      expect(challengeResult.isRight(), true);
+
+      final challenge = challengeResult.toNullable();
+      if (challenge == null) {
+        throw Exception("Challenge failed");
+      }
+
+      debugPrint(challenge.challengeId);
+      debugPrint(challenge.message);
+    });
+
+  });
+}
