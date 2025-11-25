bf9e4ea4195a84f3d13e53bfc8a2fe83f3e9f759
ogorek21 <ogorek@mooze.app>
2025-11-25 17:42:18 -0300

    feat: add levels change listner

--- lib/features/wallet_level/presentation/providers/wallet_levels_provider.dart
+++ lib/features/wallet_level/presentation/providers/wallet_levels_provider.dart
@@ -5,6 +5,7 @@ import '../../domain/entities/current_user_wallet_entity.dart';
 import '../../domain/repositories/wallet_levels_repository.dart';
 import '../../data/repositories/wallet_levels_repository_impl.dart';
 import '../../data/datasources/wallet_levels_remote_data_source.dart';
+import '../../../../shared/user/providers/user_service_provider.dart';
 
 final dioProvider = Provider<Dio>((ref) {
   return Dio();
@@ -22,31 +23,50 @@ final walletLevelsRepositoryProvider = Provider<WalletLevelsRepository>((ref) {
   return WalletLevelsRepositoryImpl(dataSource: dataSource);
 });
 
-final walletLevelsProvider = FutureProvider<List<WalletLevelEntity>>((
-  ref,
-) async {
-  final repository = ref.watch(walletLevelsRepositoryProvider);
-  return repository.getAllWalletLevels();
-});
+final walletLevelsProvider =
+    FutureProvider.autoDispose<List<WalletLevelEntity>>((ref) async {
+      final repository = ref.watch(walletLevelsRepositoryProvider);
+      return repository.getAllWalletLevels();
+    });
 
-final walletLevelByTypeProvider =
-    FutureProvider.family<WalletLevelEntity, WalletLevelType>((
-      ref,
-      type,
-    ) async {
+final walletLevelByTypeProvider = FutureProvider.autoDispose
+    .family<WalletLevelEntity, WalletLevelType>((ref, type) async {
       final repository = ref.watch(walletLevelsRepositoryProvider);
       return repository.getWalletLevelByType(type);
     });
 
-final currentUserWalletProvider = FutureProvider<CurrentUserWalletEntity>((
-  ref,
-) async {
-  // TODO: GET FROM API USER
-  await Future.delayed(const Duration(milliseconds: 500));
-  return const CurrentUserWalletEntity(
-    currentLimit: 500.0,
-    maximumPossibleLimit: 3000.0,
-    minimumLimit: 20.0,
-    currentLevel: 'Prata',
-  );
-});
+final currentUserWalletProvider =
+    FutureProvider.autoDispose<CurrentUserWalletEntity>((ref) async {
+      final userService = ref.watch(userServiceProvider);
+      final result = await userService.getUser().run();
+
+      return result.fold(
+        (error) => throw Exception('Erro ao carregar dados do usuário: $error'),
+        (user) {
+          String levelName;
+          switch (user.spendingLevel) {
+            case 0:
+              levelName = 'Bronze';
+              break;
+            case 1:
+              levelName = 'Prata';
+              break;
+            case 2:
+              levelName = 'Ouro';
+              break;
+            case 3:
+              levelName = 'Diamante';
+              break;
+            default:
+              levelName = 'Bronze';
+          }
+
+          return CurrentUserWalletEntity(
+            currentLimit: user.dailySpending,
+            maximumPossibleLimit: user.allowedSpending,
+            minimumLimit: 20.0,
+            currentLevel: levelName,
+          );
+        },
+      );
+    });
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_breez_liquid/flutter_breez_liquid.dart' as liquidSdk;
 import 'package:lwk/lwk.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mooze_mobile/themes/app_theme.dart';
 import 'package:mooze_mobile/shared/infra/sync/sync_bootstrap.dart';
 import 'package:mooze_mobile/shared/connectivity/providers/connectivity_provider.dart';
 import 'package:mooze_mobile/features/pix/presentation/widgets/pix_status_listener.dart';
 import 'package:mooze_mobile/features/wallet/presentation/widgets/transaction_status_listener.dart';
+import 'package:mooze_mobile/shared/user/widgets/level_change_listener.dart';
+import 'package:mooze_mobile/shared/user/providers/user_service_provider.dart';
 import 'routes.dart';
 
 void main() async {
@@ -16,7 +19,16 @@ void main() async {
 
   await liquidSdk.FlutterBreezLiquid.init();
 
-  runApp(ProviderScope(child: MyApp()));
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends ConsumerWidget {
@@ -24,14 +36,16 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     ref.read(walletSyncBootstrapProvider);
     ref.read(connectivityProvider);
-    return TransactionStatusListener(
-      child: PixStatusListener(
-        child: MaterialApp.router(
-          title: 'Mooze',
-          debugShowCheckedModeBanner: false,
-          theme: AppTheme.darkTheme(context),
-          themeMode: ThemeMode.dark,
-          routerConfig: router,
+    return LevelChangeListener(
+      child: TransactionStatusListener(
+        child: PixStatusListener(
+          child: MaterialApp.router(
+            title: 'Mooze',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.darkTheme(context),
+            themeMode: ThemeMode.dark,
+            routerConfig: router,
+          ),
         ),
       ),
     );
