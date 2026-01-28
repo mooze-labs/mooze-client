7059e6bdb05be951158c0be832d16db1e0b941be
ogorek21 <ogorek@mooze.app>
2026-01-27 22:43:49 -0300

    feat: update mock user data with new values and improve user retrieval logic

--- lib/shared/user/services/mock_user_service.dart
+++ lib/shared/user/services/mock_user_service.dart
@@ -21,10 +21,26 @@ class MockUserService implements UserService {
       if (_shouldFail) {
         return left('User not found');
       }
-      if (_mockUser == null) {
-        return left('User not found');
-      }
-      return right(_mockUser!);
+
+      final user =
+          _mockUser ??
+          User(
+            id: 'mock_user_id',
+            verificationLevel: 0,
+            referredBy: null,
+            allowedSpending: 25000,
+            dailySpending: 5000,
+            spendingLevel: 1,
+            levelProgress: 1,
+            valuesToReceive: {
+              '02f22f8d9c76ab41661a2729e4752e2c5d1a263012141b86ea98af5472df5189':
+                  15000000000,
+              // '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d':
+              //     25000,
+            },
+          );
+
+      return right(user);
     });
   }
 
