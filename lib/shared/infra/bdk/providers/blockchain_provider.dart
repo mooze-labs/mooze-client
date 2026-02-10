86a3bddcc4b4e0b572bd0bb59f9d80177a08187f
ogorek21 <ogorek@mooze.app>
2026-02-10 18:27:27 -0300

    chore: update URL fallback logic and adjust blockchain settings

--- lib/shared/infra/bdk/providers/blockchain_provider.dart
+++ lib/shared/infra/bdk/providers/blockchain_provider.dart
@@ -3,23 +3,28 @@ import 'package:bdk_flutter/bdk_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-const String defaultElectrumUrl = "blockstream.info:110";
+import '../utils/electrum_fallback.dart';
 
 final blockchainProvider = Provider<TaskEither<String, Blockchain>>((ref) {
   final futPrefs = Task(() => SharedPreferences.getInstance());
-  final electrumUrl = futPrefs.map(
-    (prefs) => prefs.getString('bitcoin_node_url') ?? defaultElectrumUrl,
-  );
+  final electrumUrl = futPrefs.map((prefs) {
+    final customUrl = prefs.getString('bitcoin_node_url');
+
+    if (customUrl != null) {
+      return customUrl;
+    }
+
+    return BitcoinElectrumFallback.getCurrentServer();
+  });
 
   final config = electrumUrl.map(
     (url) => BlockchainConfig.electrum(
       config: ElectrumConfig(
         url: url,
-        retry: 5,
-        stopGap: BigInt.from(10),
+        retry: 2,
+        stopGap: BigInt.from(20),
         validateDomain: false,
-        timeout: 5,
+        timeout: 15,
       ),
     ),
   );
