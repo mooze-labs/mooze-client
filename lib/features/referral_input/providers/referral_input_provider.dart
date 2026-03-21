a0842ba571d091cab952404cff4f3a0eff0d5fc9
ogorek21 <ogorek@mooze.app>
2026-03-20 23:28:58 -0300

    refactor(referral_input): remove old flat structure and update routes
    Delete legacy controllers/ and providers/ directories.
    Update settings routes import to new presentation path.

--- lib/features/referral_input/providers/referral_input_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../controllers/referral_input_controller.dart';
-import 'package:mooze_mobile/features/referral_input/providers/user_service_provider.dart';
-
-final referralInputControllerProvider =
-    StateNotifierProvider<ReferralInputController, ReferralInputState>((ref) {
-      final userService = ref.watch(userServiceProvider);
-      return ReferralInputController(userService: userService);
-    });
