271cef994783b273487badb1b2c5fc71613cdbc1
ogorek21 <ogorek@mooze.app>
2026-03-21 00:32:18 -0300

    refactor(shared): replace hardcoded colors in developer widgets with theme
    Update grid_action_button and status_item to use colorScheme instead
    of AppColors and hardcoded Colors.* values.

--- lib/shared/widgets/developer/grid_action_button.dart
+++ lib/shared/widgets/developer/grid_action_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mooze_mobile/themes/app_colors.dart';
 
 /// Botão de ação em grid para a tela de desenvolvedor
 class GridActionButton extends StatelessWidget {
@@ -27,9 +26,10 @@ class GridActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final defaultBgColor = AppColors.swapCardBackground;
-    final defaultIconColor = AppColors.primaryColor;
-    final defaultTextColor = Colors.white;
+    final colorScheme = theme.colorScheme;
+    final defaultBgColor = colorScheme.surfaceContainer;
+    final defaultIconColor = colorScheme.primary;
+    final defaultTextColor = colorScheme.onSurface;
 
     return Tooltip(
       message: tooltip,
@@ -43,13 +43,13 @@ class GridActionButton extends StatelessWidget {
               color:
                   enabled
                       ? (backgroundColor ?? defaultBgColor)
-                      : Colors.grey[800],
+                      : colorScheme.outline,
               borderRadius: BorderRadius.circular(12),
               boxShadow:
                   enabled
                       ? [
                         BoxShadow(
-                          color: Colors.black.withValues(alpha: 0.2),
+                          color: colorScheme.shadow.withValues(alpha: 0.2),
                           blurRadius: 4,
                           offset: const Offset(0, 2),
                         ),
@@ -64,7 +64,7 @@ class GridActionButton extends StatelessWidget {
                   color:
                       enabled
                           ? (iconColor ?? defaultIconColor)
-                          : Colors.grey[600],
+                          : colorScheme.outlineVariant,
                   size: 28,
                 ),
                 const SizedBox(height: 8),
@@ -74,7 +74,7 @@ class GridActionButton extends StatelessWidget {
                     color:
                         enabled
                             ? (textColor ?? defaultTextColor)
-                            : Colors.grey[600],
+                            : colorScheme.outlineVariant,
                     fontSize: 12,
                     fontWeight: FontWeight.w500,
                   ),
