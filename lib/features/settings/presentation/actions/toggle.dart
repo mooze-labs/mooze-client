ffe59723d37b5c2e04ac31bf44f8309c48c38222
ogorek21 <ogorek@mooze.app>
2026-04-18 00:45:12 -0300

    refactor(toggle): change onChange type to ValueChanged<bool> for better type safety

--- lib/features/settings/presentation/actions/toggle.dart
+++ lib/features/settings/presentation/actions/toggle.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:mooze_mobile/features/settings/presentation/actions/action.dart';
 
 class Toggle extends SettingsActions {
   final bool value;
-  final Function onChange;
+  final ValueChanged<bool> onChange;
 
   Toggle({required this.value, required this.onChange});
 
