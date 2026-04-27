c415a0563c1b7bdae0a4962a48c1922c5072c4c0
ogorek21 <ogorek@mooze.app>
2026-04-27 15:21:18 -0300

    feat(address_explorer): add entities and enums for address management

---
+++ lib/features/address_explorer/domain/entities.dart
@@ -0,0 +1,5 @@
+export 'entities/address_match.dart';
+export 'entities/address_utxo.dart';
+export 'entities/wallet_address.dart';
+export 'enums/address_chain.dart';
+export 'enums/address_status.dart';
