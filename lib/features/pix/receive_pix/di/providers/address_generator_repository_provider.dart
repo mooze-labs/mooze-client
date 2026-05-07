72b1b0acff3214a3997d00d8d662aab6fae99944
ogorek21 <ogorek@mooze.app>
2026-05-07 02:19:22 -0300

    feat(address_generator_repository): refactor provider to use WalletRepository for address generation

--- lib/features/pix/receive_pix/di/providers/address_generator_repository_provider.dart
+++ lib/features/pix/receive_pix/di/providers/address_generator_repository_provider.dart
@@ -1,16 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:fpdart/fpdart.dart';
 
+import 'package:mooze_mobile/app/di/v2_providers.dart';
 import 'package:mooze_mobile/features/pix/receive_pix/domain/repositories/address_generator_repository.dart';
 import 'package:mooze_mobile/features/pix/receive_pix/data/repositories/lwk_address_generator_repository_impl.dart';
-import 'package:mooze_mobile/shared/infra/lwk/providers.dart';
 
 final addressGeneratorRepositoryProvider =
-    FutureProvider<Either<String, AddressGeneratorRepository>>((ref) async {
-      final liquidWallet = await ref.read(liquidDataSourceProvider.future);
-
-      return liquidWallet.fold(
-        (l) => left(l),
-        (wallet) => right(LwkAddressGeneratorRepositoryImpl(wallet)),
-      );
-    });
+    FutureProvider<AddressGeneratorRepository>((ref) async {
+  final walletRepo = await ref.read(walletRepositoryProvider.future);
+  return LwkAddressGeneratorRepositoryImpl(walletRepo);
+});
