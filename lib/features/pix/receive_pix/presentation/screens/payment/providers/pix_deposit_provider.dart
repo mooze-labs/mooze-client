f74da8f348a5cf7801e526eddc8e709b4e2475c4
ogorek21 <ogorek@mooze.app>
2026-05-07 02:19:36 -0300

    feat(pix_deposit_provider): simplify deposit retrieval logic in FutureProvider

--- lib/features/pix/receive_pix/presentation/screens/payment/providers/pix_deposit_provider.dart
+++ lib/features/pix/receive_pix/presentation/screens/payment/providers/pix_deposit_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:fpdart/fpdart.dart';
 
 import 'package:mooze_mobile/features/pix/receive_pix/domain/entities/pix_deposit.dart';
@@ -7,18 +6,10 @@ import 'package:mooze_mobile/features/pix/receive_pix/presentation/providers/pix
 
 final depositDataProvider = FutureProvider.autoDispose
     .family<Either<String, PixDeposit>, String>((ref, depositId) async {
-      final controller = await ref.read(pixDepositControllerProvider.future);
-
-      return await controller.fold(
-        (err) => left("Falha ao acessar banco de dados: $err"),
-        (controller) async => await controller
-            .getDeposit(depositId)
-            .run()
-            .then(
-              (maybeDeposit) => maybeDeposit.fold(
-                (err) => left(err),
-                (deposit) => right(deposit),
-              ),
-            ),
-      );
-    });
+  final controller = await ref.read(pixDepositControllerProvider.future);
+  final result = await controller.getDeposit(depositId).run();
+  return result.fold(
+    (err) => left(err),
+    (deposit) => right(deposit),
+  );
+});
