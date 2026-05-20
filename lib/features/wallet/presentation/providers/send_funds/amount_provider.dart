20f12e3c8b7c76e88b8a80c52dec5631335fc830
ogorek21 <ogorek@mooze.app>
2026-05-20 02:46:44 -0300

    feat(amount-provider): add maxSendRequestedProvider to manage send request state

--- lib/features/wallet/presentation/providers/send_funds/amount_provider.dart
+++ lib/features/wallet/presentation/providers/send_funds/amount_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'detected_amount_provider.dart';
 
 final amountStateProvider = StateProvider<int>((ref) => 0);
+final maxSendRequestedProvider = StateProvider<bool>((ref) => false);
 
 final finalAmountProvider = Provider<int>((ref) {
   final detectedAmount = ref.watch(detectedAmountProvider);
